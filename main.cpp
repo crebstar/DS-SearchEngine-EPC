8202dc76292657fb9d9c209ae5be798eb87e7633
crebstar <crebstar.paul@yahoo.com>
2012-11-03 12:57:47 -0500

    Initial Project Commit
    Submitting net beans proj

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: crebstar
+ *
+ * Created on October 30, 2012, 6:32 PM
+ */
+
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+#include "pugixml.hpp"
+
+using namespace pugi;
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return 0;
+}
+
